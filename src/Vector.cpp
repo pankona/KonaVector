9496cf9ce69ea93e1323a405d09235b6c3a8da20
pankona <yosuke.akatsuka@gmail.com>
2015-05-23 23:02:16 +0900

    [#11] implement destructor for Vector class.

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -7,3 +7,8 @@ Vector::Vector (Point* in_start, Point* in_end) {
     start = new Point (in_start->x, in_start->y);
     end = new Point (in_end->x, in_end->y);
 }
+
+Vector::~Vector() {
+    delete start;
+    delete end;
+}
